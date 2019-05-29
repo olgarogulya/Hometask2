f30865aba0fb557e8c7e097a310c047a1578c180
olgarogulya <49169103+olgarogulya@users.noreply.github.com>
2019-05-29 18:29:50 +0300

    Update LibCreatorDAO.java

--- Hometask2_3/src/by/epam/training/hometask03/dao/LibCreatorDAO.java
+++ Hometask2_3/src/by/epam/training/hometask03/dao/LibCreatorDAO.java
@@ -1,12 +1,13 @@
 package by.epam.training.hometask03.dao;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import by.epam.training.entity.Library;
 
 public interface LibCreatorDAO {
 
-	Library create() throws FileNotFoundException;
+	Library create() throws FileNotFoundException, IOException;
 
 	void save(Library lib);
 
