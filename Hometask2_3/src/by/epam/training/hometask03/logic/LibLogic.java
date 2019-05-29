c42b7585283e443065b7be70e835add9a7d7291f
olgarogulya <49169103+olgarogulya@users.noreply.github.com>
2019-05-29 18:25:52 +0300

    Update LibLogic.java

--- Hometask2_3/src/by/epam/training/hometask03/logic/LibLogic.java
+++ Hometask2_3/src/by/epam/training/hometask03/logic/LibLogic.java
@@ -1,6 +1,7 @@
 package by.epam.training.hometask03.logic;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 import by.epam.training.hometask03.entity.Book;
@@ -8,8 +9,8 @@ import by.epam.training.hometask03.entity.Book.SortType;
 
 public interface LibLogic {
 
-	void add(Book book) throws FileNotFoundException;
-	void remove(Book book) throws FileNotFoundException;
-	List<Book> find(String author, SortType type) throws FileNotFoundException;
-	List<Book> findall() throws FileNotFoundException;
+	void add(Book book) throws FileNotFoundException, IOException;
+	void remove(Book book) throws FileNotFoundException, IOException;
+	List<Book> find(String author, SortType type) throws FileNotFoundException, IOException;
+	List<Book> findall() throws FileNotFoundException, IOException;
 }
