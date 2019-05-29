e710b6da7dd959402a9cf2cde00e17dfcdc0b813
olgarogulya <49169103+olgarogulya@users.noreply.github.com>
2019-05-29 18:28:09 +0300

    Update FileLibCreatorDAO.java

--- Hometask2_3/src/by/epam/training/hometask03/dao/impl/FileLibCreatorDAO.java
+++ Hometask2_3/src/by/epam/training/hometask03/dao/impl/FileLibCreatorDAO.java
@@ -1,8 +1,11 @@
 package by.epam.training.hometask03.dao.impl;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
+//import java.util.Scanner;
 
 import by.epam.training.hometask03.dao.LibCreatorDAO;
 import by.epam.training.hometask03.entity.Book;
@@ -10,24 +13,59 @@ import by.epam.training.hometask03.entity.Library;
 
 public class FileLibCreatorDAO implements LibCreatorDAO {
 
-	public Library create() throws FileNotFoundException {
+	public Library create() throws FileNotFoundException, IOException {
 		Library lib = new Library();
 
 		File file = new File("D:/Java_training/eclipse_workspace/Lesson15library/resources/books.txt");
-		Scanner sc = new Scanner(file);
+		
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		
+		String line;
+		BufferedReader fileReader = new BufferedReader(new FileReader(file));
+		while((line = fileReader.readLine()) != null) {
+		//	System.out.println(line = fileReader.readLine());
+			String[] str;
+			String title;
+			String author;
+			int price;
+			int yearPublished;
+			String delimiter="\\s";
+			str = line.split(delimiter);
+
+			title = str[0];
+			author = str[1];
+			price = Integer.parseInt(str[2]);
+			yearPublished = Integer.parseInt(str[3]);
+
+			Book book = new Book(title, author, price, yearPublished);
+			lib.add(book);
+			
+		}
+//System.out.println(lib);
+		return lib;
+		
+	}
+		
+		
+		
+		
+	/*	Scanner sc = new Scanner(file);
 	//	System.out.println(file);
 
 		while(sc.hasNextLine()) {
 			
 			String text = sc.nextLine();
-			//System.out.println("text " + text);
+			System.out.println("text " + text);
 
 			String[] line = text.split("\r\n|\r|\n");
-		//	System.out.println("line.length " + line.length);
-
+			//System.out.println("line.length " + line.length);
+			
 			for(int a = 0; a < line.length; a++) {
 
 				String[] str;
+				
 				String title;
 				String author;
 				int price;
@@ -49,13 +87,14 @@ public class FileLibCreatorDAO implements LibCreatorDAO {
 				
 			Book book = new Book(title, author, price, yearPublished);
 			lib.add(book);
+			//добавить save
 			//System.out.println(lib);
 			}
 
 			}
-		System.out.println(lib);
+	//	System.out.println(lib);
 		return lib;
-	}
+	}*/
 
 	@Override
 	public void save(Library lib) {
